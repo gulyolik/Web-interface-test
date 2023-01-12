03a8f62a7c6714c8d1c51f932a9cacb1f20d0ef5
gulyolik <olia.guliaeva@yandex.ru>
2023-01-12 16:44:47 +0300

    New fixes

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -1,4 +1,5 @@
 package ru.netology.web;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -22,8 +23,7 @@ public class CallbackTest {
     }
 
     @BeforeEach
-    void setUp(){
-        driver = new ChromeDriver();
+    void setUp() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
@@ -32,21 +32,21 @@ public class CallbackTest {
     }
 
     @AfterEach
-    void tearDown(){
+    void tearDown() {
         driver.quit();
         driver = null;
     }
 
     @Test
-            void shouldTestV1() throws InterruptedException{
+    void shouldTestV1() throws InterruptedException {
         driver.get("http:/localhost:9999/");
         driver.findElement(By.cssSelector("[data-test-id = name] input")).sendKeys("Василий");
         driver.findElement(By.cssSelector("[data-test-id = phone] input")).sendKeys("+79967737496");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual =driver.findElement(By.cssSelector("[data-test-id= order-success]")).getText();
-        assertEquals(expected,actual.trim());
+        String actual = driver.findElement(By.cssSelector("[data-test-id= order-success]")).getText();
+        assertEquals(expected, actual.trim());
     }
 
 }
